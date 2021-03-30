eab0006463e8adccc98880a52551dfd561d2f4d7
Francisco Carlos Moraes <moraes.junior6@gmail.com>
2021-03-30 02:58:19 -0600

    Video

--- prueba/src/main/java/com/albo/repository/CharactersRepository.java
+++ prueba/src/main/java/com/albo/repository/CharactersRepository.java
@@ -16,4 +16,7 @@ public interface CharactersRepository extends CrudRepository<CharactersModel, Lo
 	
 	@Query(value = "SELECT c.name , cr.full_name , crc.role_creator  from characters c INNER JOIN characters_comics cc ON c.marvel_Id = cc.marvel_characters_id  INNER JOIN comics co ON cc.marvel_comics_id = co.marvel_Id INNER JOIN creators_comics crc ON crc.marvel_comics_id = co.marvel_Id  INNER JOIN creators cr ON cr.marvel_Id = crc.marvel_creators_id where c.name =?1", nativeQuery = true)
 	public List<String> getCharactersWithComicsByRelacion(String name);
+	
+	@Query(value = "SELECT c.name, co.TITLE as comic_Title, co.MARVEL_ID from characters c INNER JOIN characters_comics cc ON c.marvel_Id = cc.marvel_characters_id  INNER JOIN comics co ON co.MARVEL_ID  = cc.MARVEL_COMICS_ID  WHERE  c.name = ?1 GROUP BY c.name, co.TITLE,co.MARVEL_ID", nativeQuery = true)
+	public List<String> getComicsByCharactersName(String name);
 }
