878645c0019888866a070f75e97ca222c28d9c63
Francisco Carlos Moraes <moraes.junior6@gmail.com>
2021-03-30 10:31:35 -0600

    Script

--- avengers.sh
+++ avengers.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Prueba Albo"
 
+echo "........."
+echo "Paso 1 : Inicializar Angular(Front-end)"
+echo "........."
+
 cd 'albo'
 echo "Inicializado front-end..."
 echo "Aguarde..."
